03bade9bc66610ad28026ef9376b8b4f01377026
Banana <mail@bananas-playground.net>
2021-01-06 16:08:14 +0100

    config merge into one file. missing changelog. start of develop branch

--- webclient/index.php
+++ webclient/index.php
@@ -2,7 +2,7 @@
 /**
  * Bibliotheca webclient
  *
- * Copyright 2018-2020 Johannes Keßler
+ * Copyright 2018-2021 Johannes Keßler
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,14 +16,33 @@
  * limitations under the License.
  */
 
-# set to true if you need debug messages in error log file
-define('DEBUG',false);
-# set to ture if you need query log messages in error log file.
-define('QUERY_DEBUG',false);
+require_once './config/config.php';
 
-require_once './config/path.php';
-require_once './config/system.php';
-require_once './config/database.php';
+mb_http_output('UTF-8');
+mb_internal_encoding('UTF-8');
+ini_set('error_reporting',-1); // E_ALL & E_STRICT
+
+# check request
+$_urlToParse = filter_var($_SERVER['QUERY_STRING'],FILTER_SANITIZE_STRING, FILTER_FLAG_STRIP_LOW);
+if(!empty($_urlToParse)) {
+	# see http://de2.php.net/manual/en/regexp.reference.unicode.php
+	if(preg_match('/[\p{C}\p{M}\p{Sc}\p{Sk}\p{So}\p{Zl}\p{Zp}]/u',$_urlToParse) === 1) {
+		die('Malformed request. Make sure you know what you are doing.');
+	}
+}
+
+# set the error reporting
+ini_set('log_errors',true);
+ini_set('error_log',PATH_SYSTEMOUT.'/error.log');
+if(DEBUG === true) {
+	ini_set('display_errors',true);
+}
+else {
+	ini_set('display_errors',false);
+}
+
+# time settings
+date_default_timezone_set(TIMEZONE);
 
 # static helper class
 require_once 'lib/summoner.class.php';
