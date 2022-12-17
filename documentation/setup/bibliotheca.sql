afbe36d82ff9a83604cc1be7d34df0dc73956b33
Banana <mail@bananas-playground.net>
2022-12-17 17:12:44 +0100

    sql file updates

--- documentation/setup/bibliotheca.sql
+++ documentation/setup/bibliotheca.sql
@@ -1,12 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 5.2.0
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Oct 08, 2022 at 09:54 AM
--- Server version: 8.0.27
--- PHP Version: 8.1.11
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
