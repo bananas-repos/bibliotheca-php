f239316a3ce90e7adcdf37ceec46d0fe5e96f6b4
Banana <mail@bananas-playground.net>
2025-08-30 10:45:11 +0200

    init of a new version
    Signed-off-by: Banana <mail@bananas-playground.net>

--- webclient/index.php
+++ webclient/index.php
@@ -20,7 +20,7 @@
 
 require_once './config/config.php';
 
-const BIB_VERSION = '1.8 - Dark Arena';
+const BIB_VERSION = '1.x - Harobed Village';
 
 mb_http_output('UTF-8');
 mb_internal_encoding('UTF-8');
